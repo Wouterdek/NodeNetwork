e6d5c5770878577e8eea357f2afcc3b362be9608
Wouter De Keersmaecker <wouterdekrsmckr@gmail.com>
2018-07-05 17:14:05 +0200

    Added missing derived list reactive extension method

--- NodeNetwork/Utilities/ReactiveExtensions.cs
+++ NodeNetwork/Utilities/ReactiveExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reactive.Disposables;
@@ -23,46 +24,127 @@ namespace NodeNetwork.Utilities
         /// <param name="target">The viewmodel that is used as a base for finding the target list property</param>
         /// <param name="property">The IReactiveList property that will be modified.</param>
         /// <returns>A disposable to break the binding</returns>
-        public static IDisposable BindListContents<TObj, TListItem>(this IObservable<IList<TListItem>> data, TObj target, Expression<Func<TObj, IReadOnlyReactiveList<TListItem>>> property)
+        public static IDisposable BindListContents<TObj, TListItem>(this IObservable<IList<TListItem>> data,
+            TObj target, Expression<Func<TObj, IReadOnlyReactiveList<TListItem>>> property)
         {
             IObservable<IReadOnlyReactiveList<TListItem>> targetListObservable = target.WhenAnyValue(property);
 
-            return Observable.CombineLatest(targetListObservable, data, (a, b) => (TargetList: a, SourceList: b)).Subscribe(t =>
-            {
-                IReactiveList<TListItem> latestTargetList = t.TargetList as IReactiveList<TListItem>;
-                IList<TListItem> latestData = t.SourceList;
+            return Observable.CombineLatest(targetListObservable, data, (a, b) => (TargetList: a, SourceList: b))
+                .Subscribe(t =>
+                {
+                    IReactiveList<TListItem> latestTargetList = t.TargetList as IReactiveList<TListItem>;
+                    IList<TListItem> latestData = t.SourceList;
 
-                if (latestTargetList == null)
-                {
-                    return;
-                }
-                if (latestData == null)
-                {
-                    latestTargetList.Clear();
-                    return;
-                }
-
-                var changes = LongestCommonSubsequence.GetChanges(latestTargetList, latestData).ToArray();
-                if (changes.Length == 0)
-                {
-                    return;
-                }
-                
-                using (changes.Length > 1 ? latestTargetList.SuppressChangeNotifications() : Disposable.Empty)
-                {
-                    foreach ((int index, TListItem item, LongestCommonSubsequence.ChangeType changeType) change in changes)
+                    if (latestTargetList == null)
                     {
-                        if (change.changeType == LongestCommonSubsequence.ChangeType.Removed)
+                        return;
+                    }
+
+                    if (latestData == null)
+                    {
+                        latestTargetList.Clear();
+                        return;
+                    }
+
+                    var changes = LongestCommonSubsequence.GetChanges(latestTargetList, latestData).ToArray();
+                    if (changes.Length == 0)
+                    {
+                        return;
+                    }
+
+                    using (changes.Length > 1 ? latestTargetList.SuppressChangeNotifications() : Disposable.Empty)
+                    {
+                        foreach ((int index, TListItem item, LongestCommonSubsequence.ChangeType changeType) change in
+                            changes)
                         {
-                            latestTargetList.RemoveAt(change.index);
-                        }
-                        else if (change.changeType == LongestCommonSubsequence.ChangeType.Added)
-                        {
-                            latestTargetList.Insert(change.index, change.item);
+                            if (change.changeType == LongestCommonSubsequence.ChangeType.Removed)
+                            {
+                                latestTargetList.RemoveAt(change.index);
+                            }
+                            else if (change.changeType == LongestCommonSubsequence.ChangeType.Added)
+                            {
+                                latestTargetList.Insert(change.index, change.item);
+                            }
                         }
                     }
-                }
+                });
+        }
+        
+        public static (IReadOnlyReactiveList<R> List, IDisposable Binding) CreateDerivedList<T, R>(
+            this IObservable<IReactiveList<T>> obs, Func<T, bool> filter, Func<T, R> selector)
+        {
+            ReactiveList<R> result = new ReactiveList<R>();
+            IDisposable latestBinding = null;
+
+            // Dispose the binding when we receive a null list.
+            var cleanupSubscription = obs.Where(l => l == null).Subscribe(_ => latestBinding?.Dispose());
+                
+            var bindingSubscription = obs
+                // Create a new derived list when the source list changes. (and is not null)
+                .Where(l => l != null)
+                .Select(l => l.CreateDerivedCollection(selector, filter))
+                // Mirror the contents of the derived list to result
+                .Select(list =>
+                {
+                    var binding = list.Changed.Subscribe(change =>
+                    {
+                        switch (change.Action)
+                        {
+                            case NotifyCollectionChangedAction.Add:
+                                int index = change.NewStartingIndex != -1 ? change.NewStartingIndex : result.Count;
+                                result.InsertRange(index, change.NewItems.Cast<R>());
+                                break;
+
+                            case NotifyCollectionChangedAction.Remove:
+                                if (change.OldStartingIndex != -1)
+                                {
+                                    result.RemoveRange(change.OldStartingIndex, change.OldItems.Count);
+                                }
+                                else
+                                {
+                                    result.RemoveAll(change.OldItems.Cast<R>());
+                                }
+                                break;
+
+                            case NotifyCollectionChangedAction.Replace:
+                                if (change.OldStartingIndex == change.NewStartingIndex &&
+                                    change.OldStartingIndex != -1)
+                                {
+                                    result.RemoveRange(change.OldStartingIndex, change.OldItems.Count);
+                                    result.InsertRange(change.OldStartingIndex, change.NewItems.Cast<R>());
+                                }
+                                break;
+
+                            case NotifyCollectionChangedAction.Move:
+                                result.RemoveRange(change.OldStartingIndex, change.OldItems.Count);
+                                result.InsertRange(change.NewStartingIndex, change.NewItems.Cast<R>());
+                                break;
+
+                            case NotifyCollectionChangedAction.Reset:
+                                result.Clear();
+                                break;
+
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    });
+                    return (binding, list);
+                })
+                // When the parent network changes, we will create a new derived list and so we dispose the old one.
+                .Select(t => new CompositeDisposable(t.Item1, t.Item2))
+                .Subscribe(newBinding =>
+                {
+                    latestBinding?.Dispose();
+                    latestBinding = newBinding;
+                });
+
+            var disposable = Disposable.Create(() =>
+            {
+                cleanupSubscription.Dispose();
+                bindingSubscription.Dispose();
+                latestBinding?.Dispose();
             });
+            return (result, disposable);
         }
 
         /// <summary>
