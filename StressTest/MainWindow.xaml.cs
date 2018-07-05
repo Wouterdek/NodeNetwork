8a1c5fa46b1c10466d436056435fb0dadb772ca5
Wouter De Keersmaecker <wouterdekrsmckr@gmail.com>
2018-07-05 17:16:15 +0200

    Added stress test project, useful for debugging memory leaks

---
+++ StressTest/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NodeNetwork.ViewModels;
+using NodeNetwork.Views;
+using ReactiveUI;
+
+namespace StressTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly NetworkViewModel _network;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            _network = new NetworkViewModel();
+            NetworkView.ViewModel = _network;
+        }
+        
+        public NodeViewModel CreateNode()
+        {
+            NodeInputViewModel input = new NodeInputViewModel
+            {
+                Name = "A"
+            };
+
+            NodeOutputViewModel output = new NodeOutputViewModel
+            {
+                Name = "B"
+            };
+
+            NodeViewModel node = new NodeViewModel
+            {
+                Inputs = { input },
+                Outputs = { output }
+            };
+
+            return node;
+        }
+
+        private void GenerateNodes(object sender, RoutedEventArgs e)
+        {
+            _network.Nodes.Clear();
+
+            int maxX = 10;
+            int maxY = 10;
+            for (int x = 0; x < maxX; x++)
+            {
+                for (int y = 0; y < maxY; y++)
+                {
+                    NodeViewModel node = CreateNode();
+                    node.Position = new Point(x * 200, y * 200);
+                    _network.Nodes.Add(node);
+                    Debug.WriteLine($"Added node {(x*maxY)+y}");
+                }
+            }
+        }
+        
+        private void GenerateConnections(object sender, RoutedEventArgs e)
+        {
+            var connections = _network.Nodes.Zip(_network.Nodes.Skip(1),
+                (node1, node2) => _network.ConnectionFactory(node2.Inputs[0], node1.Outputs[0]));
+            _network.Connections.AddRange(connections);
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            _network.Nodes.Clear();
+            _network.Connections.Clear();
+        }
+    }
+}
