8e4dcb8d65268e5d4de818976844e9eb6bf546e4
Wouter De Keersmaecker <wouterdekrsmckr@gmail.com>
2018-07-12 12:28:43 +0200

    Build Nuget packages with symbols and publish to SymbolSource

--- NuGet/build_nuget.bat
+++ NuGet/build_nuget.bat
@@ -1,7 +1,8 @@
 @echo off
+
 echo Building NodeNetwork NuGet package...
-nuget.exe pack ../NodeNetwork/NodeNetwork.csproj -IncludeReferencedProjects -properties Configuration=Release
+nuget.exe pack ../NodeNetwork/NodeNetwork.csproj -Symbols -IncludeReferencedProjects -properties Configuration=Release
 
 echo.
 echo Building NodeNetworkToolkit NuGet package...
-nuget.exe pack ../NodeNetworkToolkit/NodeNetworkToolkit.csproj -IncludeReferencedProjects -properties Configuration=Release
+nuget.exe pack ../NodeNetworkToolkit/NodeNetworkToolkit.csproj -Symbols -IncludeReferencedProjects -properties Configuration=Release
--- NuGet/publish.bat
+++ NuGet/publish.bat
@@ -1,5 +1,7 @@
 @echo off
 echo Push NodeNetwork to NuGet? (Didn't forget version bump?)
 pause
-nuget push NodeNetwork.2.0.0.nupkg %1 -Source https://api.nuget.org/v3/index.json
-nuget push NodeNetworkToolkit.2.0.0.nupkg %1 -Source https://api.nuget.org/v3/index.json
+nuget push NodeNetwork.3.0.1.nupkg %1 -Source https://api.nuget.org/v3/index.json
+nuget push NodeNetworkToolkit.3.0.1.nupkg %1 -Source https://api.nuget.org/v3/index.json
+nuget push NodeNetwork.3.0.1.symbols.nupkg %1 -source https://nuget.smbsrc.net/
+nuget push NodeNetworkToolkit.3.0.1.symbols.nupkg %1 -source https://nuget.smbsrc.net/
