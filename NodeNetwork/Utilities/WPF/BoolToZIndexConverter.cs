c785882e7a5e7ebcd3fda8f8306dbe4156ee1642
Wouter De Keersmaecker <wouterdekrsmckr@gmail.com>
2019-07-24 20:54:50 +0200

    Display the selected nodes on top of the other nodes.

---
+++ NodeNetwork/Utilities/WPF/BoolToZIndexConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NodeNetwork.Utilities.WPF
+{
+    public class BoolToZIndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((bool) value) ? 1 : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
