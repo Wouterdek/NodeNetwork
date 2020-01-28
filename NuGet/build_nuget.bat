6ce67f6c1b5632aedb4708501ab56f6d7a472372
Wouter <wouterdekrsmckr@gmail.com>
2020-01-28 13:29:56 +0100

    Update package properties, remove old nuget spec files, update publishing script

--- NuGet/build_nuget.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Building NodeNetwork NuGet package...
-nuget.exe pack ../NodeNetwork/NodeNetwork.csproj -Symbols -IncludeReferencedProjects -properties Configuration=Release
-
-echo.
-echo Building NodeNetworkToolkit NuGet package...
-nuget.exe pack ../NodeNetworkToolkit/NodeNetworkToolkit.csproj -Symbols -IncludeReferencedProjects -properties Configuration=Release
--- NuGet/publish.bat
+++ NuGet/publish.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Push NodeNetwork to NuGet? (Didn't forget version bump?)
 pause
-nuget push NodeNetwork.3.1.0.nupkg %1 -Source https://api.nuget.org/v3/index.json
-nuget push NodeNetworkToolkit.3.1.0.nupkg %1 -Source https://api.nuget.org/v3/index.json
-nuget push NodeNetwork.3.1.0.symbols.nupkg %1 -source https://nuget.smbsrc.net/
-nuget push NodeNetworkToolkit.3.1.0.symbols.nupkg %1 -source https://nuget.smbsrc.net/
+nuget push ../NodeNetwork/bin/Release/NodeNetwork.4.1.2.nupkg %1 -Source https://api.nuget.org/v3/index.json
+nuget push ../NodeNetworkToolkit/bin/Release/NodeNetworkToolkit.4.1.2.nupkg %1 -Source https://api.nuget.org/v3/index.json
+nuget push ../NodeNetwork/bin/Release/NodeNetwork.4.1.2.symbols.nupkg %1 -source https://nuget.smbsrc.net/
+nuget push ../NodeNetworkToolkit/bin/Release/NodeNetworkToolkit.4.1.2.symbols.nupkg %1 -source https://nuget.smbsrc.net/
