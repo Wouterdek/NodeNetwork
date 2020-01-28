e028cfa1ce30f2e4917027e07b40643b60f39e7e
Wouter <wouterdekrsmckr@gmail.com>
2020-01-28 13:42:40 +0100

    Update example packaging script

--- ExamplesPackaging/package.bat
+++ ExamplesPackaging/package.bat
@@ -4,15 +4,18 @@ mkdir NodeNetwork-Examples
 del examples.zip
 echo Copying files...
 cd NodeNetwork-Examples
-xcopy /Y "..\..\ExampleCalculatorApp\bin\Release\*.exe" "."
-xcopy /Y "..\..\ExampleCalculatorApp\bin\Release\*.exe.config" "."
-xcopy /Y "..\..\ExampleCalculatorApp\bin\Release\*.dll" "."
-xcopy /Y "..\..\ExampleShaderEditorApp\bin\Release\*.exe" "."
-xcopy /Y "..\..\ExampleShaderEditorApp\bin\Release\*.exe.config" "."
-xcopy /Y "..\..\ExampleShaderEditorApp\bin\Release\*.dll" "."
-xcopy /Y "..\..\ExampleCodeGenApp\bin\Release\*.exe" "."
-xcopy /Y "..\..\ExampleCodeGenApp\bin\Release\*.exe.config" "."
-xcopy /Y "..\..\ExampleCodeGenApp\bin\Release\*.dll" "."
+xcopy /Y "..\..\ExampleCalculatorApp\bin\Release\netcoreapp3.1\*.exe" "."
+xcopy /Y "..\..\ExampleCalculatorApp\bin\Release\netcoreapp3.1\*.json" "."
+xcopy /Y "..\..\ExampleCalculatorApp\bin\Release\netcoreapp3.1\*.exe.config" "."
+xcopy /Y "..\..\ExampleCalculatorApp\bin\Release\netcoreapp3.1\*.dll" "."
+xcopy /Y "..\..\ExampleShaderEditorApp\bin\Release\netcoreapp3.1\*.exe" "."
+xcopy /Y "..\..\ExampleShaderEditorApp\bin\Release\netcoreapp3.1\*.json" "."
+xcopy /Y "..\..\ExampleShaderEditorApp\bin\Release\netcoreapp3.1\*.exe.config" "."
+xcopy /Y "..\..\ExampleShaderEditorApp\bin\Release\netcoreapp3.1\*.dll" "."
+xcopy /Y "..\..\ExampleCodeGenApp\bin\Release\netcoreapp3.1\*.exe" "."
+xcopy /Y "..\..\ExampleCodeGenApp\bin\Release\netcoreapp3.1\*.json" "."
+xcopy /Y "..\..\ExampleCodeGenApp\bin\Release\netcoreapp3.1\*.exe.config" "."
+xcopy /Y "..\..\ExampleCodeGenApp\bin\Release\netcoreapp3.1\*.dll" "."
 cd ..
 echo Packaging examples...
 .\7zip\7z.exe a -tzip examples.zip NodeNetwork-Examples
