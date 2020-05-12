aa7eb15c7f613059c1b7e1f47ee25660d01c1d5e
Nitsche, Fabian <Fabian.Nitsche@bruker.com>
2020-05-12 10:28:39 +0200

    Added property for EndpointGroupViewModelFactory. Adjusted constructor signatures. Using AddKey instead of intermediate Bind.

---
+++ NodeNetwork/ViewModels/EndpointGroupViewModelFactory.cs
@@ -0,0 +1,18 @@
+﻿using DynamicData;
+
+namespace NodeNetwork.ViewModels
+{
+    /// <summary>
+    /// The factory method to create endpoint view models. Used in NodeViewModel.
+    /// </summary>
+    /// <param name="group">The endpoint group this view model wraps.</param>
+    /// <param name="groupEndpoints">The endpoints that belong to this groups.</param>
+    /// <param name="children">Nested endpoint groups.</param>
+    /// <param name="endpointGroupViewModelFactory">The factory method used to create the nested endpoint group view models.</param>
+    /// <returns>The view model for the endpoint group.</returns>
+    public delegate EndpointGroupViewModel EndpointGroupViewModelFactory(
+        EndpointGroup group,
+        IObservableList<Endpoint> groupEndpoints,
+        IObservableCache<Node<IGroup<Endpoint, EndpointGroup>, EndpointGroup>, EndpointGroup> children,
+        EndpointGroupViewModelFactory endpointGroupViewModelFactory);
+}
