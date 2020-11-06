dadb1e2fbf9f0cfa0fd9ed9101787fee35c1ecf0
Wouter <wouterdekrsmckr@gmail.com>
2020-11-06 12:23:21 +0100

    Update publish script

--- NuGet/publish.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Push NodeNetwork to NuGet? (Didn't forget version bump?)
-pause
-nuget push ../NodeNetwork/bin/Release/NodeNetwork.4.3.0.nupkg %1 -Source https://api.nuget.org/v3/index.json
-nuget push ../NodeNetworkToolkit/bin/Release/NodeNetworkToolkit.4.3.0.nupkg %1 -Source https://api.nuget.org/v3/index.json
-nuget push ../NodeNetwork/bin/Release/NodeNetwork.4.3.0.symbols.nupkg %1 -source https://nuget.smbsrc.net/
-nuget push ../NodeNetworkToolkit/bin/Release/NodeNetworkToolkit.4.3.0.symbols.nupkg %1 -source https://nuget.smbsrc.net/
