717ada0d0ed038491c48bef83ed9fb4daad8045f
Wouter <wouterdekrsmckr@gmail.com>
2020-11-14 13:56:46 +0100

    Version bump

--- NuGet/publish.ps1
+++ NuGet/publish.ps1
@@ -1,4 +1,4 @@
-$version = "4.3.0"
+$version = "5.1.0"
 
 if (![System.IO.File]::Exists("nuget.exe")) {
     Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "nuget.exe"
@@ -6,7 +6,7 @@ if (![System.IO.File]::Exists("nuget.exe")) {
 
 Write-Output ("Push NodeNetwork " + $version + " to NuGet? (Didn't forget version bump?)")
 Read-Host
-nuget.exe push ("../NodeNetwork/bin/Release/NodeNetwork." + $version + ".nupkg") $args[0] -Source https://api.nuget.org/v3/index.json
-nuget.exe push ("../NodeNetworkToolkit/bin/Release/NodeNetworkToolkit." + $version + ".nupkg") $args[0] -Source https://api.nuget.org/v3/index.json
-nuget.exe push ("../NodeNetwork/bin/Release/NodeNetwork." + $version + ".symbols.nupkg") $args[0] -source https://nuget.smbsrc.net/
-nuget.exe push ("../NodeNetworkToolkit/bin/Release/NodeNetworkToolkit." + $version + ".symbols.nupkg") $args[0] -source https://nuget.smbsrc.net/
+./nuget.exe push ("../NodeNetwork/bin/Release/NodeNetwork." + $version + ".nupkg") $args[0] -Source https://api.nuget.org/v3/index.json
+./nuget.exe push ("../NodeNetworkToolkit/bin/Release/NodeNetworkToolkit." + $version + ".nupkg") $args[0] -Source https://api.nuget.org/v3/index.json
+./nuget.exe push ("../NodeNetwork/bin/Release/NodeNetwork." + $version + ".symbols.nupkg") $args[0] -source https://nuget.smbsrc.net/
+./nuget.exe push ("../NodeNetworkToolkit/bin/Release/NodeNetworkToolkit." + $version + ".symbols.nupkg") $args[0] -source https://nuget.smbsrc.net/
