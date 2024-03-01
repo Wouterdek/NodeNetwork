38a4956f28ccec0b36e11e57916c880933c70f4e
hui.yin02 <hui.yin02@united-imaging.com>
2024-03-01 17:37:40 +0800

    test

--- NodeNetwork/Properties/AssemblyInfo.cs
+++ NodeNetwork/Properties/AssemblyInfo.cs
@@ -23,6 +23,9 @@ using System.Windows;
 
 
 
+
+
+
 //In order to begin building localizable applications, set
 //<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
 //inside a <PropertyGroup>.  For example, if you are using US english
